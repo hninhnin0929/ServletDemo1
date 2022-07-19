52555fdad88aeabf79d3c0ff7a6a75f5ab1b8888
Hnin Hnin <hh@mit.com.mm>
2022-07-19 16:31:32 +0630

    first commit

---
+++ src/com/welcome/HelloServlet.java
@@ -0,0 +1,121 @@
+package com.welcome;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+//	public void init(ServletConfig config) throws ServletException {
+//		// TODO Auto-generated method stub
+//	}
+//
+//	/**
+//	 * @see Servlet#destroy()
+//	 */
+//	public void destroy() {
+//		// TODO Auto-generated method stub
+//	}
+//
+//	/**
+//	 * @see Servlet#getServletInfo()
+//	 */
+//	public String getServletInfo() {
+//		// TODO Auto-generated method stub
+//		return null; 
+//	}
+//
+//	/**
+//	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+        response.setContentType("text/html");
+        
+        PrintWriter out = response.getWriter();
+        String uname = request.getParameter("val1");
+        String pw = request.getParameter("val2");
+        
+//        out.print("<html><body>");
+//        out.print("<h2>Welcome to GeeksForGeeks</h2>");
+//        out.print("</body></html>");
+        out.println("User Details Page:");
+        out.println("Hello "+ uname);
+        out.println("Your Password is **"+ pw +"**");
+        out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+//	 */
+//	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+//	 */
+//	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doHead(HttpServletRequest, HttpServletResponse)
+//	 */
+//	protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doOptions(HttpServletRequest, HttpServletResponse)
+//	 */
+//	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doTrace(HttpServletRequest, HttpServletResponse)
+//	 */
+//	protected void doTrace(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//	}
+
+}
